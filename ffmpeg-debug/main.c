545e74652d53e85f5796991d3e6df7266910a211
steve <wangchengquan@qiyi.com>
2020-05-06 11:43:58 +0800

    Initial Commit

---
+++ ffmpeg-debug/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ffmpeg-debug
+//
+//  Created by steve on 2020/5/6.
+//  Copyright © 2020 steve. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
